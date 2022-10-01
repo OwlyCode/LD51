b756d4aa9e59109471d99e90578bdf10d0751bb6
Tristan Maindron <tmaindron@gmail.com>
2022-10-01 19:09:10 +0200

    Animated character

--- Player.gd
+++ Player.gd
@@ -4,10 +4,12 @@ const ALIGN_CORRECTION_FORCE = 100000
 
 func _ready():
 	lock_rotation = true
+	$AnimatedSprite2d.play("Run")
 
 func _process(delta):
 	if not $VisibleOnScreenNotifier2d.is_on_screen():
 		get_tree().reload_current_scene()
+
 
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
@@ -21,3 +23,8 @@ func _physics_process(delta):
 		if $LeftGroundChecker.is_colliding() or $RightGroundChecker.is_colliding():
 			apply_central_impulse(Vector2.UP * 25000)
 			$JumpSound.play()
+
+	if $LeftGroundChecker.is_colliding() or $RightGroundChecker.is_colliding():
+		$AnimatedSprite2d.play("Run")
+	else:
+		$AnimatedSprite2d.play("Jump")
